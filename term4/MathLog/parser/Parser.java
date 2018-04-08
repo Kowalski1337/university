c60c874721cb32051c9eb60624f29e02b00c9b04
Kowalski1337 <33070907+Kowalski1337@users.noreply.github.com>
2018-04-08 17:16:18 +0300

    Parser First Try

--- term4/MathLog/parser/Parser.java
+++ term4/MathLog/parser/Parser.java
@@ -1,5 +1,10 @@
 package parser;
 
+import expression.BinaryOperator;
+import expression.Expression;
+import expression.UnaryOperator;
+import expression.Variable;
+
 public class Parser {
     int index = 0;
     private String expression;
@@ -29,7 +34,85 @@ public class Parser {
                 cur = Token.IMPL;
                 index++;
                 break;
-                c
+            case '(':
+                cur = Token.LPAR;
+                break;
+            case ')':
+                cur = Token.RPAR;
+                break;
+            case '!':
+                cur = Token.NOT;
+                break;
+            default: {
+                StringBuilder sb = new StringBuilder();
+                while (Character.isLetterOrDigit(ch)) {
+                    sb.append(ch);
+                    ch = getNextChar();
+                }
+                index--;
+                variable = sb.toString();
+                index--;
+            }
         }
+    }
+
+    private Expression parseUnary() {
+        getNext();
+        Expression ans;
+        switch (cur) {
+            case NOT:
+                ans = new UnaryOperator(parseUnary(), "!");
+                break;
+            case VAR:
+                ans = new Variable(variable);
+                break;
+            case LPAR:
+                ans = parseImpl();
+                getNext();
+                break;
+            default:
+                ans = null;
+        }
+        return ans;
+    }
+
+    Expression parseAnd() {
+        Expression ans = parseUnary();
+        while (true) {
+            if (cur == Token.AND) {
+                ans = new BinaryOperator(ans, parseUnary(), "&");
+            } else {
+                return ans;
+            }
+        }
+    }
+
+    Expression parseOr() {
+        Expression ans = parseAnd();
+        while (true) {
+            if (cur == Token.OR) {
+                ans = new BinaryOperator(ans, parseAnd(), "|");
+            } else {
+                return ans;
+            }
+        }
+    }
+
+    Expression parseImpl() {
+        Expression ans = parseOr();
+        while (true) {
+            if (cur == Token.IMPL) {
+                ans = new BinaryOperator(ans, parseOr(), "->");
+            } else {
+                return ans;
+            }
+        }
+    }
+
+    public Expression parse(String expression) {
+        this.expression = expression;
+        Expression ans = parseImpl();
+        index = 0;
+        return ans;
     }
 }
