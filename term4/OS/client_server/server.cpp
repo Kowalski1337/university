e1e528fef623dfe5ae070fe89ab2ba637895ab90
Kowalski1337 <vadem1998@mail.ru>
2018-05-06 22:51:44 +0200

    simple client-server app added, app with quotes of great Itmo teachers will be soon

--- term4/OS/client_server/server.cpp
+++ term4/OS/client_server/server.cpp
@@ -19,7 +19,7 @@ int main() {
     }
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(7777);
+    addr.sin_port = htons(1337);
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     if (bind(listener, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
@@ -41,11 +41,12 @@ int main() {
 
         while (true) {
             printf("(Server)Waiting...\n");
-            char* buf = static_cast<char *>(malloc(sizeof(char)));
+            char* buf = static_cast<char *>(malloc(sizeof(char)*100));
             bytes_read = static_cast<int>(recv(sock, buf, 2048, 0));
             if (bytes_read <= 0) break;
             if (!strcmp(buf, "0")){
                 close(sock);
+                free(buf);
                 close(listener);
                 return 0;
             };
