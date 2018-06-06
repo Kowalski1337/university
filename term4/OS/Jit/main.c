bfee12913a62ad6c60f2978371935c40afc6d1b8
Kowalski1337 <vadem1998@mail.ru>
2018-06-06 22:45:19 +0200

    hard mode added

--- term4/OS/Jit/main.c
+++ term4/OS/Jit/main.c
@@ -16,14 +16,23 @@ void error(char *message) {
 
 /**
  * Compile machine code int runtime
+ * machine code increment arg1 by arg2
+ * arg1 - int
+ * arg2 - [0..15]
  * @return
  */
 int main(int argN, char **args) {
-    if (argN < 2) {
+    if (argN < 3) {
         error("Not enough args\n");
     }
-    char machine_code[] = {0x55, 0x48, 0x89, 0xe5, 0x89, 0x7d, 0xfc, 0x8b, 0x45, 0xfc, 0xba, 0x01, 0x00, 0x00, 0x00,
-                           0x89, 0xc1, 0xd3, 0xe2, 0x89, 0xd0, 0x5d, 0xc3};
+
+    int arg1 = atoll(args[1]);
+    int arg2 = atoll(args[2]);
+
+    char machine_code[] = {0x55, 0x48, 0x89, 0xe5, 0x89, 0x7d, 0xfc, 0x8b, 0x45, 0xfc, 0x83, 0xc0, 0x00 , 0x5d, 0xc3};
+    char to_hex[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    machine_code[12] = to_hex[arg2];
+
     void *mem = mmap(NULL, sizeof(machine_code), PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1,
                      0);
     if (mem == MAP_FAILED) {
@@ -34,7 +43,7 @@ int main(int argN, char **args) {
     if (mprotect(mem, sizeof(machine_code), PROT_READ | PROT_EXEC) == -1) {
         error("Problems was occurred while calling mprotect\n");
     }
-    int c = ((int (*)(int)) mem)(atoll(args[1]));
+    int c = ((int (*)(int)) mem)(arg1);
     printf("%d\n", c);
 
     if (munmap(mem, sizeof(machine_code)) == -1) {
